2cc765805da68f1b9cd24e6b6fe9212ff88d58bd
walter27 <walter952012@gmail.com>
2026-02-28 09:11:01 -0500

    fix:change url front

--- dist/appointment/server/angular-app-manifest.mjs
+++ dist/appointment/server/angular-app-manifest.mjs
@@ -13,16 +13,16 @@ export default {
   {
     "renderMode": 2,
     "preload": [
-      "chunk-G6VF4D7W.js"
+      "chunk-P4N5ZM63.js"
     ],
     "route": "/calendar"
   }
 ],
   entryPointToBrowserMapping: undefined,
   assets: {
-    'index.csr.html': {size: 5833, hash: '967971f3055b7a412bd5326259111afaad4b81c08cd7dad89b693813c1672540', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
-    'index.server.html': {size: 1102, hash: '622580a5ca56ce20b7fb82b005581fac3a51667162a876edfa7b870ec61ad6b5', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
-    'calendar/index.html': {size: 103165, hash: 'f137e89f75b32e4b9d622c9f7cd99d82d99d7b6e17e2d50a510792a08c1aa0a5', text: () => import('./assets-chunks/calendar_index_html.mjs').then(m => m.default)},
+    'index.csr.html': {size: 5833, hash: 'fd512be1be9cc43c84de1c14d8ef70837db9212f973e54604ba7662c2d8186fe', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
+    'index.server.html': {size: 1102, hash: 'ca5ed9fe177425ffe4ea89da7d8dcac2ba12cc855250d64626dceb5b04e61466', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
+    'calendar/index.html': {size: 118635, hash: 'c109dfd5d3b6de8be5abfa4d632c62c1cdd7c48da40a68bd8e7281dd4a88a92e', text: () => import('./assets-chunks/calendar_index_html.mjs').then(m => m.default)},
     'styles-GWPWU33U.css': {size: 27843, hash: 'Tg9ztFdAhVI', text: () => import('./assets-chunks/styles-GWPWU33U_css.mjs').then(m => m.default)}
   },
 };
